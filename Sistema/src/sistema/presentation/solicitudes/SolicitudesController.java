05dc4330cbf663a4fb6cdf2a98cf8e1fc2d13fbd
Rache <rbasulto1141@hotmail.com>
2018-11-05 21:57:23 -0600

    mmm

--- Sistema/src/sistema/presentation/solicitudes/SolicitudesController.java
+++ Sistema/src/sistema/presentation/solicitudes/SolicitudesController.java
@@ -185,30 +185,30 @@ public class SolicitudesController {
         Usuario principal = (Usuario) sesion.getAttribute("Usuario"); //si es true, no es registrador, si es false si es registrador
        
         if (!Arrays.asList(Application.REGISTRADOR_BIENES).contains(principal.getUsuarioRol())){ //verifica si el rol del usuario es de registrador de bienes
-            return false;
+            return true;
         }
         else
-            return true;
+            return false;
     } 
     
     public boolean permisoSecretario(){
         Usuario principal = (Usuario) sesion.getAttribute("Usuario"); //si es secretaria es true, si no es, es false
        
         if (Arrays.asList(Application.SECRETARIA).contains(principal.getUsuarioRol())){ //verifica si el rol del usuario es secretaria
-            return false;
+            return true;
         }
         else
-            return true;
+            return false;
     }
     
     public boolean permisoJefeOCCB(){
         Usuario principal = (Usuario) sesion.getAttribute("Usuario"); //si es secretaria es true, si no es, es false
        
         if (Arrays.asList(Application.JEFE_OCCB).contains(principal.getUsuarioRol())){ //verifica si el rol del usuario es secretaria
-            return false;
+            return true;
         }
         else
-            return true;
+            return false;
     }
     
     public boolean getSession(){
